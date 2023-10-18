405d8473b7c7a703daab82b445a33f418b9fbc72
Kyle Trinh <kyle.trinh94@gmail.com>
2023-10-18 07:47:57 -0700

    :bug: Fix the 404 bug on Home Page

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -11,7 +11,6 @@ import {
 	CardTitle,
 } from '#app/components/ui/card.tsx'
 import { prisma } from '#app/utils/db.server.ts'
-import { invariantResponse } from '#app/utils/misc.tsx'
 
 export const meta: MetaFunction = () => [{ title: 'Vi Comm' }]
 
@@ -30,42 +29,43 @@ export async function loader() {
 		},
 	})
 
-	invariantResponse(listingCategories.length, 'No listing categories found', {
-		status: 404,
-	})
-
 	return json({ listingCategories })
 }
 
 export default function Index() {
-	const data = useLoaderData<typeof loader>()
+	const { listingCategories } = useLoaderData<typeof loader>()
+
+	if (!listingCategories.length)
+		return (
+			<div className="container flex h-full items-center justify-center">
+				<h2 className="text-6xl">Nothing to show here</h2>
+			</div>
+		)
 
 	return (
 		<div className="container grid grid-cols-3 gap-4">
-			{data.listingCategories.map(
-				({ id, slug, title, description, _count }) => (
-					<Card key={id} className="flex flex-col justify-between">
-						<div>
-							<CardHeader>
-								<CardTitle>
-									<Link to={`/category/${slug}`}>
-										{title} ({_count.listings})
-									</Link>
-								</CardTitle>
-							</CardHeader>
-							<CardContent>{description}</CardContent>
-						</div>
-						<CardFooter>
-							<Link
-								to={`/category/${slug}`}
-								className={buttonVariants({ className: 'w-full' })}
-							>
-								See More
-							</Link>
-						</CardFooter>
-					</Card>
-				),
-			)}
+			{listingCategories.map(({ id, slug, title, description, _count }) => (
+				<Card key={id} className="flex flex-col justify-between">
+					<div>
+						<CardHeader>
+							<CardTitle>
+								<Link to={`/category/${slug}`}>
+									{title} ({_count.listings})
+								</Link>
+							</CardTitle>
+						</CardHeader>
+						<CardContent>{description}</CardContent>
+					</div>
+					<CardFooter>
+						<Link
+							to={`/category/${slug}`}
+							className={buttonVariants({ className: 'w-full' })}
+						>
+							See More
+						</Link>
+					</CardFooter>
+				</Card>
+			))}
 		</div>
 	)
 }
